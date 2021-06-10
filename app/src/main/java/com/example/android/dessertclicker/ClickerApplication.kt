f5e7eec7a3af71004d27d0df6b360edca627870e
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-06-10 14:33:27 +0700

    refactor: initialize Timber library

--- app/src/main/java/com/example/android/dessertclicker/ClickerApplication.kt
+++ app/src/main/java/com/example/android/dessertclicker/ClickerApplication.kt
@@ -1,6 +1,13 @@
 package com.example.android.dessertclicker
 
 import android.app.Application
+import timber.log.Timber
 
 class ClickerApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // initialize Timber library
+        Timber.plant(Timber.DebugTree())
+    }
 }
